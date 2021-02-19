a6ec4f654862f95ae155ad5b17386d718c350005
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-02-19 05:15:48 +0000

    Update actions/setup-node action to v2

--- pkg/models/account.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Account struct {
-	tableName string `pg:"accounts"`
-
-	AccountId uint64 `json:"accountId" pg:"account_id,notnull,pk,type:'bigserial'"`
-	Timezone  string `json:"timezone" pg:"timezone,notnull,default:'UTC'"`
-}
