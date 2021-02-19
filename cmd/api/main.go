e6de890597845970d9c742451acad038640138bb
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-02-18 20:13:10 -0600

    Updated pipeline. Removed gitlab registry.

---
+++ cmd/api/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/harderthanitneedstobe/rest-api/v0/pkg/application"
+	"github.com/harderthanitneedstobe/rest-api/v0/pkg/cache"
+	"github.com/harderthanitneedstobe/rest-api/v0/pkg/jobs"
+	"github.com/sirupsen/logrus"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/harderthanitneedstobe/rest-api/v0/pkg/config"
+	"github.com/harderthanitneedstobe/rest-api/v0/pkg/controller"
+)
+
+func main() {
+	configuration := config.LoadConfiguration()
+
+	log := logrus.NewEntry(logrus.StandardLogger())
+
+	db := pg.Connect(&pg.Options{
+		Addr: fmt.Sprintf("%s:%d",
+			configuration.PostgreSQL.Address,
+			configuration.PostgreSQL.Port,
+		),
+		User:            configuration.PostgreSQL.Username,
+		Password:        configuration.PostgreSQL.Password,
+		Database:        configuration.PostgreSQL.Database,
+		ApplicationName: "harder - api",
+	})
+
+	redisController, err := cache.NewRedisCache(log, configuration.Redis)
+	if err != nil {
+		panic(err)
+	}
+	defer redisController.Close()
+
+	job := jobs.NewJobManager(log, redisController.Pool(), db)
+	defer job.Close()
+
+	c := controller.NewController(configuration, db)
+
+	app := application.NewApp(configuration, c)
+
+	app.Listen(":4000")
+}
