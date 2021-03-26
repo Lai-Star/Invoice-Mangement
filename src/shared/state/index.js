c8c335e239837526f189d3378670f0cca1ba057f
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-03-25 20:49:58 -0500

    Progress on ui and spending.

---
+++ src/shared/state/index.js
@@ -0,0 +1,18 @@
+import { combineReducers } from "redux";
+import authentication from 'shared/authentication/reducer';
+import bankAccounts from 'shared/bankAccounts/reducer';
+import bootstrap from 'shared/bootstrap/reducer';
+import expenses from 'shared/spending/reducer';
+import links from 'shared/links/reducer';
+import transactions from 'shared/transactions/reducer';
+
+export default combineReducers({
+  authentication,
+  bankAccounts,
+  bootstrap,
+  expenses,
+  links,
+  transactions,
+});
+
+
---
+++ src/shared/transactions/state.ts
@@ -0,0 +1,12 @@
+import { Map, OrderedMap } from "immutable";
+import Transaction from 'data/Transaction';
+
+export default class TransactionState {
+  items: Map<number, OrderedMap<number, Transaction>>;
+  loaded: boolean;
+  loading: boolean;
+
+  constructor() {
+    this.items = Map<number, OrderedMap<number, Transaction>>();
+  }
+}
