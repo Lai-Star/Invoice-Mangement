8fa23355accd649e2813175d09ca6f9109084867
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-04-05 02:59:42 +0000

    Update dependency eslint-plugin-jest to v24.3.4

---
+++ src/shared/spending/state.ts
@@ -0,0 +1,14 @@
+import Spending from "data/Spending";
+import { Map } from 'immutable';
+
+export default class SpendingState {
+  items: Map<number, Map<number, Spending>>;
+  loaded: boolean;
+  loading: boolean;
+
+  selectedExpenseId: number|null;
+
+  constructor() {
+    this.items = Map<number, Map<number, Spending>>();
+  }
+}
