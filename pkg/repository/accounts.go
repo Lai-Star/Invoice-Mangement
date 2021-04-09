7e53b1670908a573380b4b6844aa48412dbfe929
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-04-09 17:41:33 -0500

    Fixes and improvements for spending + transfers.

---
+++ pkg/repository/accounts.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"github.com/harderthanitneedstobe/rest-api/v0/pkg/models"
+	"github.com/pkg/errors"
+)
+
+func (r *repositoryBase) GetAccount() (*models.Account, error) {
+	if r.account != nil {
+		return r.account, nil
+	}
+
+	var account models.Account
+	err := r.txn.Model(&account).
+		Where(`"account"."account_id" = ?`, r.AccountId()).
+		Limit(1).
+		Select(&account)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to retrieve account")
+	}
+
+	r.account = &account
+
+	return r.account, nil
+}
