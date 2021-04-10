2b55319760d558cf4581250befb003453f71b56d
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-04-10 16:36:21 -0500

    Starting to add get transactions for spending endpoint.

---
+++ pkg/repository/expenses.go
@@ -0,0 +1,72 @@
+package repository
+
+import (
+	"github.com/harderthanitneedstobe/rest-api/v0/pkg/models"
+	"github.com/pkg/errors"
+)
+
+func (r *repositoryBase) GetSpending(bankAccountId uint64) ([]models.Spending, error) {
+	var result []models.Spending
+	err := r.txn.Model(&result).
+		Where(`"spending"."account_id" = ?`, r.AccountId()).
+		Where(`"spending"."bank_account_id" = ?`, bankAccountId).
+		Select(&result)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to retrieve spending")
+	}
+
+	return result, nil
+}
+
+func (r *repositoryBase) GetExpensesByFundingSchedule(bankAccountId, fundingScheduleId uint64) ([]models.Spending, error) {
+	result := make([]models.Spending, 0)
+	err := r.txn.Model(&result).
+		Where(`"spending"."account_id" = ?`, r.AccountId()).
+		Where(`"spending"."bank_account_id" = ?`, bankAccountId).
+		Where(`"spending"."funding_schedule_id" = ?`, fundingScheduleId).
+		Where(`"spending"."spending_type" = ?`, models.SpendingTypeExpense).
+		Select(&result)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to retrieve expenses for funding schedule")
+	}
+
+	return result, nil
+}
+
+func (r *repositoryBase) CreateSpending(spending *models.Spending) error {
+	spending.AccountId = r.AccountId()
+
+	_, err := r.txn.Model(spending).Insert(spending)
+	return errors.Wrap(err, "failed to create spending")
+}
+
+// UpdateExpenses should only be called with complete expense models. Do not use partial models with missing data for
+// this action.
+func (r *repositoryBase) UpdateExpenses(bankAccountId uint64, updates []models.Spending) error {
+	for i := range updates {
+		updates[i].AccountId = r.AccountId()
+	}
+
+	_, err := r.txn.Model(&updates).
+		Update(&updates)
+	if err != nil {
+		return errors.Wrap(err, "failed to update expenses")
+	}
+
+	return nil
+}
+
+func (r *repositoryBase) GetSpendingById(bankAccountId, spendingId uint64) (*models.Spending, error) {
+	var result models.Spending
+	err := r.txn.Model(&result).
+		Relation("FundingSchedule").
+		Where(`"spending"."account_id" = ?`, r.AccountId()).
+		Where(`"spending"."bank_account_id" = ?`, bankAccountId).
+		Where(`"spending"."spending_id" = ?`, spendingId).
+		Select(&result)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to retrieve expense")
+	}
+
+	return &result, nil
+}
--- src/shared/bankAccounts/selectors/getBankAccountsByLinkId.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from "reselect";
-import { getBankAccounts } from "shared/bankAccounts/selectors/getBankAccounts";
-
-export const getBankAccountsByLinkId = linkId => createSelector(
-  [getBankAccounts],
-  bankAccounts => bankAccounts.filter(bankAccount => bankAccount.linkId === linkId)
-);
