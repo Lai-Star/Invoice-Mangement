de8a3b63e8b009b6b62ae0096a00c4fdf4d52ffd
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-04-09 22:57:38 -0500

    Getting started on goals.

---
+++ src/data/Balance.ts
@@ -0,0 +1,26 @@
+export default class Balance {
+  bankAccountId: number;
+  available: number;
+  current: number;
+  safe: number;
+  expenses: number;
+  goals: number;
+
+  constructor(data?: Partial<Balance>) {
+    if (data) {
+      Object.assign(this, data)
+    }
+  }
+
+  getSafeToSpendString(): string {
+    return `$${ (this.safe / 100).toFixed(2) }`;
+  }
+
+  getAvailableString(): string {
+    return `$${ (this.available / 100).toFixed(2) }`;
+  }
+
+  getExpensesString(): string {
+    return `$${ (this.expenses / 100).toFixed(2) }`;
+  }
+}
