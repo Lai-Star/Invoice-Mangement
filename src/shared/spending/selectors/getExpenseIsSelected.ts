089d7ab995668eda935ded17abb8ff495876ac33
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-04-19 18:02:58 -0500

    Improvements to the helm chart.

--- src/shared/spending/selectors/getExpenseIsSelected.ts
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const getSelectedExpenseId = state => state.spending.selectedExpenseId;
-
-export const getExpenseIsSelected = (expenseId: number) => createSelector<any, any, boolean>(
-  [getSelectedExpenseId],
-  selectedExpenseId => expenseId === selectedExpenseId,
-);
