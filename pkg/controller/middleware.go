4eb00f8b24227026c872e437e4d508bdd58a1378
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-04-26 17:19:59 -0500

    Adding some stuff for tracing

---
+++ pkg/controller/middleware.go
@@ -0,0 +1,170 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/go-pg/pg/v10"
+	"github.com/kataras/iris/v12/context"
+	"github.com/monetrapp/rest-api/pkg/repository"
+	"github.com/pkg/errors"
+)
+
+const (
+	databaseContextKey  = "_harderDatabase_"
+	accountIdContextKey = "_accountId_"
+	userIdContextKey    = "_userId_"
+	loginIdContextKey   = "_loginId_"
+	spanContextKey      = "_span_"
+)
+
+func (c *Controller) setupRepositoryMiddleware(ctx *context.Context) {
+	var cleanup func()
+	var dbi pg.DBI
+	switch ctx.Method() {
+	case "GET", "OPTIONS":
+		dbi = c.db
+	case "POST", "PUT", "DELETE":
+		txn, err := c.db.BeginContext(ctx.Request().Context())
+		if err != nil {
+			c.wrapAndReturnError(ctx, err, http.StatusInternalServerError, "failed to begin transaction")
+			return
+		}
+
+		cleanup = func() {
+			// TODO (elliotcourant) Add proper logging here that the request has failed
+			//  and we are rolling back the transaction.
+			if ctx.GetErr() != nil {
+				if err := txn.RollbackContext(ctx.Request().Context()); err != nil {
+					// Rollback
+					c.log.WithError(err).Errorf("failed to rollback request")
+				}
+			} else {
+				if err = txn.CommitContext(ctx.Request().Context()); err != nil {
+					// failed to commit
+					fmt.Println(err)
+				}
+			}
+		}
+
+		dbi = txn
+	}
+
+	ctx.Values().Set(databaseContextKey, dbi)
+
+	ctx.Next()
+
+	if cleanup != nil {
+		cleanup()
+	}
+
+	ctx.Next()
+}
+
+func (c *Controller) authenticationMiddleware(ctx *context.Context) {
+	var token string
+
+	token = ctx.GetCookie("M-Token", context.CookieSecure)
+	if token == "" {
+		token = ctx.GetHeader(TokenName)
+	}
+
+	if token == "" {
+		c.returnError(ctx, http.StatusForbidden, "unauthorized")
+		return
+	}
+
+	var claims HarderClaims
+	result, err := jwt.ParseWithClaims(token, &claims, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(c.configuration.JWT.LoginJwtSecret), nil
+	})
+	if err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusForbidden, "unauthorized")
+		return
+	}
+
+	if !result.Valid {
+		c.returnError(ctx, http.StatusForbidden, "unauthorized")
+		return
+	}
+
+	ctx.Values().Set(accountIdContextKey, claims.AccountId)
+	ctx.Values().Set(userIdContextKey, claims.UserId)
+	ctx.Values().Set(loginIdContextKey, claims.LoginId)
+
+	ctx.Next()
+}
+
+func (c *Controller) loggingMiddleware(ctx *context.Context) {
+	ctx.Next()
+
+	if err := ctx.GetErr(); err != nil {
+		c.log.WithContext(ctx.Request().Context()).WithError(err).Errorf("%+v", ctx.GetErr())
+	}
+}
+
+func (c *Controller) getUnauthenticatedRepository(ctx *context.Context) (repository.UnauthenticatedRepository, error) {
+	txn, ok := ctx.Values().Get(databaseContextKey).(*pg.Tx)
+	if !ok {
+		return nil, errors.Errorf("no transaction for request")
+	}
+
+	return repository.NewUnauthenticatedRepository(txn), nil
+}
+
+func (c *Controller) mustGetUserId(ctx *context.Context) uint64 {
+	userId := ctx.Values().GetUint64Default(userIdContextKey, 0)
+	if userId == 0 {
+		panic("unauthorized")
+	}
+
+	return userId
+}
+
+func (c *Controller) mustGetAccountId(ctx *context.Context) uint64 {
+	accountId := ctx.Values().GetUint64Default(accountIdContextKey, 0)
+	if accountId == 0 {
+		panic("unauthorized")
+	}
+
+	return accountId
+}
+
+func (c *Controller) getAuthenticatedRepository(ctx *context.Context) (repository.Repository, error) {
+	loginId := ctx.Values().GetUint64Default(loginIdContextKey, 0)
+	if loginId == 0 {
+		return nil, errors.Errorf("not authorized")
+	}
+
+	userId := ctx.Values().GetUint64Default(userIdContextKey, 0)
+	if userId == 0 {
+		return nil, errors.Errorf("you are not authenticated to an account")
+	}
+
+	accountId := ctx.Values().GetUint64Default(accountIdContextKey, 0)
+	if accountId == 0 {
+		return nil, errors.Errorf("you are not authenticated to an account")
+	}
+
+	txn, ok := ctx.Values().Get(databaseContextKey).(pg.DBI)
+	if !ok {
+		return nil, errors.Errorf("no transaction for request")
+	}
+
+	return repository.NewRepositoryFromSession(userId, accountId, txn), nil
+}
+
+func (c *Controller) mustGetAuthenticatedRepository(ctx *context.Context) repository.Repository {
+	repo, err := c.getAuthenticatedRepository(ctx)
+	if err != nil {
+		panic("unauthorized")
+	}
+
+	return repo
+}
---
+++ pkg/controller/plaid_webhook.go
@@ -0,0 +1,66 @@
+package controller
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/kataras/iris/v12/context"
+)
+
+type PlaidWebhook struct {
+	WebhookType         string                 `json:"webhook_type"`
+	WebhookCode         string                 `json:"webhook_code"`
+	ItemId              string                 `json:"item_id"`
+	Error               map[string]interface{} `json:"error"`
+	NewWebhookURL       string                 `json:"new_webhook_url"`
+	NewTransactions     int64                  `json:"new_transactions"`
+	RemovedTransactions []string               `json:"removed_transactions"`
+}
+
+func (c *Controller) handlePlaidWebhook(ctx *context.Context) {
+	verification := ctx.GetHeader("Plaid-Verification")
+	if strings.TrimSpace(verification) == "" {
+		c.returnError(ctx, http.StatusUnauthorized, "unauthorized")
+		return
+	}
+
+	// TODO Properly verify webhooks from Plaid.
+
+	var hook PlaidWebhook
+	if err := ctx.ReadJSON(&hook); err != nil {
+		c.badRequest(ctx, "malformed JSON")
+		return
+	}
+
+	if err := c.processWebhook(hook); err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusInternalServerError, "failed to handle webhook")
+		return
+	}
+}
+
+func (c *Controller) processWebhook(hook PlaidWebhook) error {
+	switch hook.WebhookType {
+	case "TRANSACTIONS":
+		switch hook.WebhookCode {
+		case "INITIAL_UPDATE":
+
+		}
+
+	}
+
+	return nil
+}
+
+func (c *Controller) getWebhookUrl() string {
+	if !c.configuration.EnableWebhooks {
+		return ""
+	}
+
+	uri, err := url.Parse(c.configuration.APIDomainName)
+	if err != nil {
+		panic(err)
+	}
+
+	return uri.String()
+}
