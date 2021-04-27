cdc677abdfa57fa1c1884d183655e420795d03bd
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-04-27 08:31:54 -0500

    Messing around with wrapping iris in a common interface.

---
+++ pkg/controller/register.go
@@ -0,0 +1,279 @@
+package controller
+
+import (
+	"context"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/getsentry/sentry-go"
+	"github.com/kataras/iris/v12"
+	"github.com/monetrapp/rest-api/pkg/hash"
+	"github.com/pkg/errors"
+	"github.com/stripe/stripe-go/v72"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type RegistrationClaims struct {
+	RegistrationId string `json:"registrationId"`
+	jwt.StandardClaims
+}
+
+func (c *Controller) registerEndpoint(ctx iris.Context) {
+	goCtx := ctx.Request().Context()
+	span := sentry.StartSpan(goCtx, "register", sentry.TransactionName("POST /authentication/register"))
+	defer span.Finish()
+	var registerRequest struct {
+		Email     string `json:"email"`
+		Password  string `json:"password"`
+		FirstName string `json:"firstName"`
+		LastName  string `json:"lastName"`
+		Timezone  string `json:"timezone"`
+		Captcha   string `json:"captcha"`
+	}
+	if err := ctx.ReadJSON(&registerRequest); err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusBadRequest, "invalid register JSON")
+		return
+	}
+
+	// This will take the captcha from the request and validate it if the API is
+	// configured to do so. If it is enabled and the captcha fails then an error
+	// is returned to the client.
+	if err := c.validateCaptchaMaybe(goCtx, registerRequest.Captcha); err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusBadRequest, "valid ReCAPTCHA is required")
+		return
+	}
+
+	registerRequest.Email = strings.TrimSpace(registerRequest.Email)
+	registerRequest.Password = strings.TrimSpace(registerRequest.Password)
+	registerRequest.FirstName = strings.TrimSpace(registerRequest.FirstName)
+
+	if err := c.validateRegistration(
+		registerRequest.Email,
+		registerRequest.Password,
+		registerRequest.FirstName,
+	); err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusBadRequest,
+			"invalid registration",
+		)
+		return
+	}
+
+	timezone, err := time.LoadLocation(registerRequest.Timezone)
+	if err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusBadRequest, "failed to parse timezone")
+		return
+	}
+
+	// TODO (elliotcourant) Add stuff to verify email address by sending an
+	//  email.
+
+	if c.configuration.Stripe.Enabled {
+		name := registerRequest.FirstName + " " + registerRequest.LastName
+		_, err := c.stripeClient.Customers.New(&stripe.CustomerParams{
+			Email:               &registerRequest.Email,
+			Name:                &name,
+		})
+		if err != nil {
+			c.wrapAndReturnError(ctx, err, http.StatusInternalServerError, "failed to create stripe customer")
+			return
+		}
+
+		// TODO Add stripe customer details to the login/user record.
+	}
+
+	// If the registration details provided look good then we want to create an
+	// unauthenticated repository. This will give us some basic database access
+	// without being able to access user information directly. It is essentially
+	// a write only interface to the database.
+	repository, err := c.getUnauthenticatedRepository(ctx)
+	if err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusInternalServerError,
+			"cannot register user",
+		)
+		return
+	}
+
+	// Hash the user's password so that we can store it securely.
+	hashedPassword := hash.HashPassword(
+		registerRequest.Email, registerRequest.Password,
+	)
+
+	// Create the user's login record in the database, this will return the login
+	// record including the new login's loginId which we will need below. If SMTP
+	// is enabled and we want to verify emails then the user is disabled
+	// initially.
+	login, err := repository.CreateLogin(
+		registerRequest.Email,
+		hashedPassword,
+		!(c.configuration.SMTP.Enabled && c.configuration.SMTP.VerifyEmails),
+	)
+	if err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusInternalServerError,
+			"failed to create login",
+		)
+		return
+	}
+
+	// Now that the login exists we can create the account, at the time of
+	// writing this we are only using the local time zone of the server, but in
+	// the future I want to have it somehow use the user's timezone.
+	account, err := repository.CreateAccount(timezone)
+	if err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusInternalServerError,
+			"failed to create account",
+		)
+		return
+	}
+
+	// Now that we have an accountId we can create the user object which will
+	// bind the login and the account together.
+	user, err := repository.CreateUser(
+		login.LoginId,
+		account.AccountId,
+		registerRequest.FirstName,
+		registerRequest.LastName,
+	)
+	if err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusInternalServerError,
+			"failed to create user",
+		)
+		return
+	}
+
+	// If SMTP is enabled and we are verifying emails then we want to create a
+	// registration record and send the user a verification email.
+	if c.configuration.SMTP.Enabled && c.configuration.SMTP.VerifyEmails {
+		ctx.JSON(map[string]interface{}{
+			"needsVerification": true,
+		})
+		return
+	}
+
+	// If we are not requiring email verification to activate an account we can
+	// simply return a token here for the user to be signed in.
+	token, err := c.generateToken(login.LoginId, user.UserId, account.AccountId)
+	if err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusInternalServerError,
+			"failed to create JWT",
+		)
+		return
+	}
+
+	user.Login = login
+	user.Account = account
+
+	ctx.JSON(map[string]interface{}{
+		"token": token,
+		"user":  user,
+	})
+}
+
+func (c *Controller) verifyEndpoint(ctx iris.Context) {
+	var verifyRequest struct {
+		Token string `json:"token"`
+	}
+	if err := ctx.ReadJSON(&verifyRequest); err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusBadRequest, "malformed json")
+		return
+	}
+
+	var claims RegistrationClaims
+	result, err := jwt.ParseWithClaims(verifyRequest.Token, &claims, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(c.configuration.JWT.RegistrationJwtSecret), nil
+	})
+	if err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusForbidden, "unauthorized")
+		return
+	}
+
+	if !result.Valid {
+		c.returnError(ctx, http.StatusForbidden, "unauthorized")
+		return
+	}
+
+	repo, err := c.getUnauthenticatedRepository(ctx)
+	if err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusInternalServerError, "failed to get repository")
+		return
+	}
+
+	user, err := repo.VerifyRegistration(claims.RegistrationId)
+	if err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusInternalServerError, "failed to verify registration")
+		return
+	}
+
+	token, err := c.generateToken(user.LoginId, user.UserId, user.AccountId)
+	if err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusInternalServerError,
+			"failed to create JWT",
+		)
+		return
+	}
+
+	ctx.JSON(map[string]interface{}{
+		"token": token,
+		"user":  user,
+	})
+}
+
+func (c *Controller) validateRegistration(email, password, firstName string) error {
+	if email == "" {
+		return errors.Errorf("email cannot be blank")
+	}
+
+	if len(password) < 8 {
+		return errors.Errorf("password must be at least 8 characters")
+	}
+
+	if firstName == "" {
+		return errors.Errorf("first name cannot be blank")
+	}
+
+	return nil
+}
+
+func (c *Controller) validateCaptchaMaybe(ctx context.Context, captcha string) error {
+	if !c.configuration.ReCAPTCHA.Enabled {
+		// If it is disabled then we don't need to do anything.
+		return nil
+	}
+
+	span := sentry.StartSpan(ctx, "validate ReCAPTCHA")
+	defer span.Finish()
+
+	if captcha == "" {
+		return errors.Errorf("captcha is not valid")
+	}
+
+	return c.captcha.Verify(captcha)
+}
+
+func (c *Controller) generateRegistrationToken(registrationId string) (string, error) {
+	now := time.Now()
+	claims := &RegistrationClaims{
+		RegistrationId: registrationId,
+		StandardClaims: jwt.StandardClaims{
+			Audience:  c.configuration.APIDomainName,
+			ExpiresAt: now.Add(7 * 24 * time.Hour).Unix(),
+			Id:        "",
+			IssuedAt:  now.Unix(),
+			Issuer:    c.configuration.APIDomainName,
+			NotBefore: now.Unix(),
+			Subject:   "harderThanItNeedsToBe - registration",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(c.configuration.JWT.RegistrationJwtSecret))
+	if err != nil {
+		return "", errors.Wrap(err, "failed to sign registration JWT")
+	}
+
+	return signedToken, nil
+}
---
+++ pkg/repository/repository.go
@@ -0,0 +1,119 @@
+package repository
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/monetrapp/rest-api/pkg/models"
+	"github.com/pkg/errors"
+)
+
+type Repository interface {
+	AccountId() uint64
+	UserId() uint64
+
+	CreateBankAccounts(bankAccounts ...models.BankAccount) error
+	CreateSpending(expense *models.Spending) error
+	CreateFundingSchedule(fundingSchedule *models.FundingSchedule) error
+	CreateLink(link *models.Link) error
+	CreatePlaidLink(link *models.PlaidLink) error
+	CreateTransaction(bankAccountId uint64, transaction *models.Transaction) error
+	GetAccount() (*models.Account, error)
+	GetBalances(ctx context.Context, bankAccountId uint64) (*Balances, error)
+	GetBankAccount(bankAccountId uint64) (*models.BankAccount, error)
+	GetBankAccounts() ([]models.BankAccount, error)
+	GetBankAccountsByLinkId(linkId uint64) ([]models.BankAccount, error)
+	GetSpendingById(bankAccountId, expenseId uint64) (*models.Spending, error)
+	GetSpending(ctx context.Context, bankAccountId uint64) ([]models.Spending, error)
+	GetSpendingByFundingSchedule(bankAccountId, fundingScheduleId uint64) ([]models.Spending, error)
+	GetFundingSchedule(bankAccountId, fundingScheduleId uint64) (*models.FundingSchedule, error)
+	GetFundingSchedules(bankAccountId uint64) ([]models.FundingSchedule, error)
+	GetIsSetup() (bool, error)
+	GetJob(jobId string) (models.Job, error)
+	GetLink(linkId uint64) (*models.Link, error)
+	GetLinkIsManual(linkId uint64) (bool, error)
+	GetLinkIsManualByBankAccountId(bankAccountId uint64) (bool, error)
+	GetLinks() ([]models.Link, error)
+	GetMe() (*models.User, error)
+	GetPendingTransactionsForBankAccount(bankAccountId uint64) ([]models.Transaction, error)
+	GetTransaction(bankAccountId, transactionId uint64) (*models.Transaction, error)
+	GetTransactions(bankAccountId uint64, limit, offset int) ([]models.Transaction, error)
+	GetTransactionsByPlaidId(linkId uint64, plaidTransactionIds []string) (map[string]TransactionUpdateId, error)
+	InsertTransactions(transactions []models.Transaction) error
+	UpdateBankAccounts(accounts []models.BankAccount) error
+	UpdateExpenses(bankAccountId uint64, updates []models.Spending) error
+	UpdateTransaction(bankAccountId uint64, transaction *models.Transaction) error
+	UpdateLink(link *models.Link) error
+	UpdateNextFundingScheduleDate(fundingScheduleId uint64, nextOccurrence time.Time) error
+}
+
+type UnauthenticatedRepository interface {
+	CreateLogin(email, hashedPassword string, isEnabled bool) (*models.Login, error)
+	CreateAccount(timezone *time.Location) (*models.Account, error)
+	CreateUser(loginId, accountId uint64, firstName, lastName string) (*models.User, error)
+
+	// VerifyRegistration takes a registrationId and will finalize the registration record. If the registration has
+	// already been completed an error is returned.
+	VerifyRegistration(registrationId string) (*models.User, error)
+}
+
+func NewRepositoryFromSession(userId, accountId uint64, database pg.DBI) Repository {
+	return &repositoryBase{
+		userId:    userId,
+		accountId: accountId,
+		txn:       database,
+	}
+}
+
+func NewUnauthenticatedRepository(txn *pg.Tx) UnauthenticatedRepository {
+	return &unauthenticatedRepo{
+		txn: txn,
+	}
+}
+
+var (
+	_ Repository = &repositoryBase{}
+)
+
+func (r *repositoryBase) UserId() uint64 {
+	return r.userId
+}
+
+func (r *repositoryBase) AccountId() uint64 {
+	return r.accountId
+}
+
+func (r *repositoryBase) GetMe() (*models.User, error) {
+	var user models.User
+	err := r.txn.Model(&user).
+		Relation("Login").
+		Relation("Account").
+		Where(`"user"."user_id" = ? AND "user"."account_id" = ?`, r.userId, r.accountId).
+		Limit(1).
+		Select(&user)
+	switch err {
+	case pg.ErrNoRows:
+		return nil, errors.Errorf("user does not exist")
+	case nil:
+		break
+	default:
+		return nil, errors.Wrapf(err, "failed to retrieve user")
+	}
+
+	return &user, nil
+}
+
+func (r *repositoryBase) GetIsSetup() (bool, error) {
+	return r.txn.Model(&models.Link{}).
+		Where(`"link"."account_id" = ?`, r.accountId).
+		Exists()
+}
+
+func (r *repositoryBase) GetBankAccounts() ([]models.BankAccount, error) {
+	var result []models.BankAccount
+	err := r.txn.Model(&result).
+		Where(`"bank_account"."account_id" = ?`, r.AccountId()).
+		Select(&result)
+	return result, errors.Wrap(err, "failed to retrieve bank accounts")
+}
