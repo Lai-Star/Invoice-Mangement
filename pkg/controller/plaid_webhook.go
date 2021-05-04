aae5728a28ae11d6478880992c825d6d21c2e2c3
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-05-03 19:52:39 -0500

    Added a way to view the gocraft/work dashboard locally.

---
+++ pkg/controller/plaid_webhook.go
@@ -0,0 +1,52 @@
+package controller
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/kataras/iris/v12/context"
+)
+
+type PlaidWebhook struct {
+	WebhookType         string                 `json:"webhook_type"`
+	WebhookCode         string                 `json:"webhook_code"`
+	ItemId              string                 `json:"item_id"`
+	Error               map[string]interface{} `json:"error"`
+	NewWebhookURL       string                 `json:"new_webhook_url"`
+	NewTransactions     int64                  `json:"new_transactions"`
+	RemovedTransactions []string               `json:"removed_transactions"`
+}
+
+func (c *Controller) handlePlaidWebhook(ctx *context.Context) {
+	verification := ctx.GetHeader("Plaid-Verification")
+	if strings.TrimSpace(verification) == "" {
+		c.returnError(ctx, http.StatusUnauthorized, "unauthorized")
+		return
+	}
+
+	// TODO Properly verify webhooks from Plaid.
+
+	var hook PlaidWebhook
+	if err := ctx.ReadJSON(&hook); err != nil {
+		c.badRequest(ctx, "malformed JSON")
+		return
+	}
+
+	if err := c.processWebhook(hook); err != nil {
+		c.wrapAndReturnError(ctx, err, http.StatusInternalServerError, "failed to handle webhook")
+		return
+	}
+}
+
+func (c *Controller) processWebhook(hook PlaidWebhook) error {
+	switch hook.WebhookType {
+	case "TRANSACTIONS":
+		switch hook.WebhookCode {
+		case "INITIAL_UPDATE":
+
+		}
+
+	}
+
+	return nil
+}
