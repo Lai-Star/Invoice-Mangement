19c81d75d91c87fe4e14362e9dd9b6f649b79a0b
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-05-13 16:52:22 -0500

    Working on stripe interface, as well as tests for it.

--- src/shared/bankAccounts/selectors/getSelectedBankAccountId.ts
+++
@@ -1,4 +0,0 @@
-// @ts-ignore
-export const getSelectedBankAccountId = (state: any): number | null => {
-  return state?.bankAccounts?.selectedBankAccountId || +window.localStorage.getItem('selectedBankAccountId') || null;
-};
