f1f36d02bebe60c7d7aa0060dad4862dc69c22df
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-05-13 10:39:12 -0500

    Run a basic JS build on pull requests

---
+++ src/shared/bootstrap/state.js
@@ -0,0 +1,17 @@
+import { Record } from 'immutable';
+
+export default class BootstrapState extends Record({
+  apiUrl: '',
+  isReady: false,
+  isBootstrapping: true,
+  verifyLogin: false,
+  verifyRegister: false,
+  requireLegalName: false,
+  requirePhoneNumber: false,
+  ReCAPTCHAKey: null,
+  allowSignUp: false,
+  allowForgotPassword: false,
+  requireBetaCode: false,
+}) {
+
+}
