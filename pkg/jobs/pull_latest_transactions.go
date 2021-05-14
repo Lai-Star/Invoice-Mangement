522dc1ec9aee906c464e299285dadaec3bb99165
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-05-14 08:51:48 -0500

    Fixing timezones for transaction dates properly.

---
+++ pkg/jobs/pull_latest_transactions.go
@@ -0,0 +1,285 @@
+package jobs
+
+import (
+	"context"
+	"github.com/getsentry/sentry-go"
+	"github.com/gocraft/work"
+	"github.com/monetrapp/rest-api/pkg/models"
+	"github.com/monetrapp/rest-api/pkg/repository"
+	"github.com/monetrapp/rest-api/pkg/util"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"strconv"
+	"time"
+)
+
+const (
+	EnqueuePullLatestTransactions = "EnqueuePullLatestTransactions"
+	PullLatestTransactions        = "PullLatestTransactions"
+)
+
+func (j *jobManagerBase) TriggerPullLatestTransactions(accountId, linkId uint64, numberOfTransactions int64) (jobId string, err error) {
+	log := j.log.WithFields(logrus.Fields{
+		"accountId": accountId,
+		"linkId":    linkId,
+	})
+
+	log.Infof("queueing pull latest transactions for account")
+	job, err := j.queue.EnqueueUnique(PullLatestTransactions, map[string]interface{}{
+		"accountId":            accountId,
+		"linkId":               linkId,
+		"numberOfTransactions": numberOfTransactions,
+	})
+	if err != nil {
+		log.WithError(err).Error("failed to enqueue pulling latest transactions")
+		return "", errors.Wrap(err, "failed to enqueue pulling latest transactions")
+	}
+	log = log.WithField("pullLatestTransactionsJobId", job.ID)
+
+	log.Infof("queueing account balances update for account")
+	job, err = j.queue.EnqueueUnique(PullAccountBalances, map[string]interface{}{
+		"accountId": accountId,
+		"linkId":    linkId,
+	})
+	if err != nil {
+		log.WithError(err).Error("failed to enqueue pulling account balances")
+		return "", errors.Wrap(err, "failed to enqueue pulling account balances")
+	}
+
+	return job.ID, nil
+}
+
+func (j *jobManagerBase) enqueuePullLatestTransactions(job *work.Job) error {
+	log := j.getLogForJob(job)
+
+	accounts, err := j.getPlaidLinksByAccount()
+	if err != nil {
+		log.WithError(err).Errorf("failed to retrieve bank accounts that need to by synced")
+		return err
+	}
+
+	log.Infof("enqueing %d account(s) to pull latest transactions", len(accounts))
+
+	for _, account := range accounts {
+		for _, linkId := range account.LinkIDs {
+			accountLog := log.WithFields(logrus.Fields{
+				"accountId": account.AccountID,
+				"linkId":    linkId,
+			})
+			accountLog.Trace("enqueueing for latest transactions update")
+			_, err := j.queue.EnqueueUnique(PullLatestTransactions, map[string]interface{}{
+				"accountId": account.AccountID,
+				"linkId":    linkId,
+			})
+			if err != nil {
+				accountLog.WithError(err).Error("could not enqueue account, data will not be synced")
+				continue
+			}
+
+			accountLog.Trace("successfully enqueued account for latest transactions update")
+		}
+	}
+
+	return nil
+}
+
+func (j *jobManagerBase) pullLatestTransactions(job *work.Job) error {
+	span := sentry.StartSpan(context.Background(), "Job", sentry.TransactionName("Pull Latest Transactions"))
+	defer span.Finish()
+
+	start := time.Now()
+	log := j.getLogForJob(job)
+	log.Infof("pulling account balances")
+
+	accountId, err := j.getAccountId(job)
+	if err != nil {
+		log.WithError(err).Error("could not run job, no account Id")
+		return err
+	}
+
+	defer func() {
+		if j.stats != nil {
+			j.stats.JobFinished(PullAccountBalances, accountId, start)
+		}
+	}()
+
+	linkId := uint64(job.ArgInt64("linkId"))
+	span.SetTag("linkId", strconv.FormatUint(linkId, 10))
+	span.SetTag("accountId", strconv.FormatUint(accountId, 10))
+
+	return j.getRepositoryForJob(job, func(repo repository.Repository) error {
+		account, err := repo.GetAccount()
+		if err != nil {
+			log.WithError(err).Error("failed to retrieve account for job")
+			return err
+		}
+
+		timezone, err := account.GetTimezone()
+		if err != nil {
+			log.WithError(err).Warn("failed to get account's time zone, defaulting to UTC")
+			timezone = time.UTC
+		}
+
+		link, err := repo.GetLink(span.Context(), linkId)
+		if err != nil {
+			log.WithError(err).Error("failed to retrieve link details to pull transactions")
+			return err
+		}
+
+		if link.PlaidLink == nil {
+			err = errors.Errorf("cannot pull account balanaces for link without plaid info")
+			log.WithError(err).Errorf("failed to pull transactions")
+			return err
+		}
+
+		bankAccounts, err := repo.GetBankAccountsByLinkId(linkId)
+		if err != nil {
+			log.WithError(err).Error("failed to retrieve bank account details to pull transactions")
+			return err
+		}
+
+		// Gather the plaid account Ids so we can precisely query plaid.
+		plaidIdsToBankIds := map[string]uint64{}
+		itemBankAccountIds := make([]string, len(bankAccounts))
+		for i, bankAccount := range bankAccounts {
+			itemBankAccountIds[i] = bankAccount.PlaidAccountId
+			plaidIdsToBankIds[bankAccount.PlaidAccountId] = bankAccount.BankAccountId
+		}
+
+		log.Debugf("retrieving transactions for %d bank account(s)", len(itemBankAccountIds))
+
+		transactions, err := j.plaidClient.GetAllTransactions(
+			span.Context(),
+			link.PlaidLink.AccessToken,
+			time.Now().Add(-7*24*time.Hour),
+			time.Now(),
+			itemBankAccountIds,
+		)
+		if err != nil {
+			log.WithError(err).Error("failed to retrieve transactions from plaid")
+			return errors.Wrap(err, "failed to retrieve transactions from plaid")
+		}
+
+		// TODO Are plaid transaction Ids unique per link, or per bank account?
+		//  If they are not then this could cause an issue where a user's checking transaction has the same Id as a
+		//  savings account transaction but under the same link. Causing the transaction to get updated improperly.
+		plaidTransactionIds := make([]string, len(transactions))
+		for i, transaction := range transactions {
+			plaidTransactionIds[i] = transaction.ID
+		}
+
+		transactionsByPlaidId, err := repo.GetTransactionsByPlaidId(linkId, plaidTransactionIds)
+		if err != nil {
+			log.WithError(err).Error("failed to retrieve transaction ids for updating plaid transactions")
+			return err
+		}
+
+		transactionsToUpdate := make([]*models.Transaction, 0)
+		transactionsToInsert := make([]models.Transaction, 0)
+		now := time.Now().UTC()
+		for _, plaidTransaction := range transactions {
+			amount := int64(plaidTransaction.Amount * 100)
+
+			date, _ := util.ParseInLocal("2006-01-02", plaidTransaction.Date, timezone)
+			var authorizedDate *time.Time
+			if plaidTransaction.AuthorizedDate != "" {
+				authDate, _ := util.ParseInLocal("2006-01-02", plaidTransaction.AuthorizedDate, timezone)
+				authorizedDate = &authDate
+			}
+
+			var pendingPlaidTransactionId *string
+			if plaidTransaction.PendingTransactionID != "" {
+				pendingPlaidTransactionId = &plaidTransaction.PendingTransactionID
+			}
+
+			transactionName := plaidTransaction.Name
+			if plaidTransaction.MerchantName != "" {
+				transactionName = plaidTransaction.MerchantName
+			}
+
+			existingTransaction, ok := transactionsByPlaidId[plaidTransaction.ID]
+			if !ok {
+				transactionsToInsert = append(transactionsToInsert, models.Transaction{
+					AccountId:                 accountId,
+					BankAccountId:             plaidIdsToBankIds[plaidTransaction.AccountID],
+					PlaidTransactionId:        plaidTransaction.ID,
+					Amount:                    amount,
+					SpendingId:                nil,
+					Spending:                  nil,
+					Categories:                plaidTransaction.Category,
+					OriginalCategories:        plaidTransaction.Category,
+					Date:                      date,
+					AuthorizedDate:            authorizedDate,
+					Name:                      transactionName,
+					OriginalName:              plaidTransaction.Name,
+					MerchantName:              plaidTransaction.MerchantName,
+					OriginalMerchantName:      plaidTransaction.MerchantName,
+					IsPending:                 plaidTransaction.Pending,
+					CreatedAt:                 now,
+					PendingPlaidTransactionId: pendingPlaidTransactionId,
+				})
+				continue
+			}
+
+			var shouldUpdate bool
+			if existingTransaction.Amount != amount {
+				shouldUpdate = true
+			}
+
+			if existingTransaction.IsPending != plaidTransaction.Pending {
+				shouldUpdate = true
+			}
+
+			if existingTransaction.AuthorizedDate == nil && authorizedDate != nil {
+				shouldUpdate = true
+			} else if existingTransaction.AuthorizedDate != nil && authorizedDate != nil && !existingTransaction.AuthorizedDate.Equal(*authorizedDate) {
+				shouldUpdate = true
+			}
+
+			if existingTransaction.PendingPlaidTransactionId != pendingPlaidTransactionId {
+				shouldUpdate = true
+			}
+
+			existingTransaction.Amount = amount
+			existingTransaction.IsPending = plaidTransaction.Pending
+			existingTransaction.AuthorizedDate = authorizedDate
+			existingTransaction.PendingPlaidTransactionId = pendingPlaidTransactionId
+
+			// Update old records if we see them to use the merchant name by default.
+			if existingTransaction.Name == plaidTransaction.Name {
+				existingTransaction.Name = transactionName
+				shouldUpdate = true
+			}
+
+			// Fix timezone of records.
+			if existingTransaction.Date != date {
+				existingTransaction.Date = date
+				shouldUpdate = true
+			}
+
+			if shouldUpdate {
+				transactionsToUpdate = append(transactionsToUpdate, &existingTransaction)
+			}
+		}
+
+		if len(transactionsToUpdate) > 0 {
+			if err = repo.UpdateTransactions(span.Context(), transactionsToUpdate); err != nil {
+				log.WithError(err).Errorf("failed to update transactions for job")
+				return err
+			}
+		}
+
+		if len(transactionsToInsert) > 0 {
+			// Reverse the list so the oldest records are inserted first.
+			for i, j := 0, len(transactionsToInsert)-1; i < j; i, j = i+1, j-1 {
+				transactionsToInsert[i], transactionsToInsert[j] = transactionsToInsert[j], transactionsToInsert[i]
+			}
+			if err = repo.InsertTransactions(transactionsToInsert); err != nil {
+				log.WithError(err).Error("failed to insert new transactions")
+				return err
+			}
+		}
+
+		return nil
+	})
+}
---
+++ pkg/models/spending.go
@@ -0,0 +1,120 @@
+package models
+
+import (
+	"github.com/monetrapp/rest-api/pkg/util"
+	"github.com/pkg/errors"
+	"time"
+)
+
+type SpendingType uint8
+
+const (
+	SpendingTypeExpense SpendingType = iota
+	SpendingTypeGoal
+)
+
+type Spending struct {
+	tableName string `pg:"spending"`
+
+	SpendingId             uint64           `json:"spendingId" pg:"spending_id,notnull,pk,type:'bigserial'"`
+	AccountId              uint64           `json:"-" pg:"account_id,notnull,pk,on_delete:CASCADE,type:'bigint'"`
+	Account                *Account         `json:"-" pg:"rel:has-one"`
+	BankAccountId          uint64           `json:"bankAccountId" pg:"bank_account_id,notnull,pk,unique:per_bank,on_delete:CASCADE,type:'bigint'"`
+	BankAccount            *BankAccount     `json:"bankAccount,omitempty" pg:"rel:has-one" swaggerignore:"true"`
+	FundingScheduleId      uint64           `json:"fundingScheduleId" pg:"funding_schedule_id,notnull,on_delete:RESTRICT"`
+	FundingSchedule        *FundingSchedule `json:"-" pg:"rel:has-one" swaggerignore:"true"`
+	SpendingType           SpendingType     `json:"spendingType" pg:"spending_type,notnull,use_zero,unique:per_bank"`
+	Name                   string           `json:"name" pg:"name,notnull,unique:per_bank"`
+	Description            string           `json:"description,omitempty" pg:"description"`
+	TargetAmount           int64            `json:"targetAmount" pg:"target_amount,notnull,use_zero"`
+	CurrentAmount          int64            `json:"currentAmount" pg:"current_amount,notnull,use_zero"`
+	UsedAmount             int64            `json:"usedAmount" pg:"used_amount,notnull,use_zero"`
+	RecurrenceRule         *Rule            `json:"recurrenceRule" pg:"recurrence_rule,type:'text'" swaggertype:"string"`
+	LastRecurrence         *time.Time       `json:"lastRecurrence" pg:"last_recurrence"`
+	NextRecurrence         time.Time        `json:"nextRecurrence" pg:"next_recurrence,notnull"`
+	NextContributionAmount int64            `json:"nextContributionAmount" pg:"next_contribution_amount,notnull,use_zero"`
+	IsBehind               bool             `json:"isBehind" pg:"is_behind,notnull,use_zero"`
+	IsPaused               bool             `json:"isPaused" pg:"is_paused,notnull,use_zero"`
+	DateCreated            time.Time        `json:"dateCreated" pg:"date_created,notnull"`
+}
+
+func (e Spending) GetProgressAmount() int64 {
+	switch e.SpendingType {
+	case SpendingTypeGoal:
+		return e.CurrentAmount + e.UsedAmount
+	case SpendingTypeExpense:
+		fallthrough
+	default:
+		return e.CurrentAmount
+	}
+}
+
+func (e *Spending) CalculateNextContribution(
+	accountTimezone string,
+	nextContributionDate time.Time,
+	nextContributionRule *Rule,
+) error {
+	timezone, err := time.LoadLocation(accountTimezone)
+	if err != nil {
+		return errors.Wrap(err, "failed to parse account's timezone")
+	}
+
+	// The total needed needs to be calculated differently for goals and expenses. How much expenses need is always a
+	// representation of the target amount minus the current amount allocated to the expense. But goals work a bit
+	// differently because the allocated amount can fluctuate throughout the life of the goal. When a transaction is
+	// spent from a goal it deducts from the current amount, but adds to the used amount. This is to keep track of how
+	// much the goal has actually progress while maintaining existing patterns for calculating allocations. As a result
+	// for us to know how much a goal needs, we need to subtract the current amount plus the used amount from the target
+	// for goals.
+	progressAmount := e.GetProgressAmount()
+
+	nextContributionDate = util.MidnightInLocal(nextContributionDate, timezone)
+
+	// If we have achieved our expense then we don't need to do anything.
+	if e.TargetAmount <= progressAmount {
+		e.IsBehind = false
+		e.NextContributionAmount = 0
+	}
+
+	nextDueDate := util.MidnightInLocal(e.NextRecurrence, timezone)
+	if time.Now().After(nextDueDate) && e.RecurrenceRule != nil {
+		e.LastRecurrence = &nextDueDate
+		e.NextRecurrence = util.MidnightInLocal(e.RecurrenceRule.After(nextDueDate, false), timezone)
+		nextDueDate = util.MidnightInLocal(e.NextRecurrence, timezone)
+	}
+
+	// If the next time we would contribute to this expense is after the next time the expense is due, then the expense
+	// has fallen behind. Mark it as behind and set the contribution to be the difference.
+	if nextContributionDate.After(nextDueDate) {
+		e.NextContributionAmount = e.TargetAmount - progressAmount
+		e.IsBehind = progressAmount < e.TargetAmount
+		return nil
+	} else if nextContributionDate.Equal(nextDueDate) {
+		// If the next time we would contribute is the same day it's due, this is okay. The user could change the due
+		// date if they want a bit of a buffer and we would plan it differently. But we don't want to consider this
+		// "behind".
+		e.IsBehind = false
+		e.NextContributionAmount = e.TargetAmount - progressAmount
+		return nil
+	} else if progressAmount >= e.TargetAmount {
+		e.IsBehind = false
+	}
+
+	// If the next time we would contribute to this expense is not behind and has more than one contribution to meet its
+	// target then we need to calculate a partial contribution.
+	numberOfContributions := 0
+	if nextContributionDate.Before(nextDueDate) {
+		numberOfContributions++
+	}
+
+	// TODO Handle expenses that recur more frequently than they are funded.
+	nowInTimezone := time.Now().In(timezone)
+	nextContributionRule.DTStart(nowInTimezone)
+	numberOfContributions = len(nextContributionRule.Between(nowInTimezone, nextDueDate, false))
+
+	totalNeeded := e.TargetAmount - progressAmount
+	perContribution := totalNeeded / int64(numberOfContributions)
+
+	e.NextContributionAmount = perContribution
+	return nil
+}
