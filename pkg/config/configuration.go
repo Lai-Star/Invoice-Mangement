12148639156bcbc17230eaffd81ed3d6c807c1e1
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-05-15 20:32:04 -0500

    Starting on selecting billing, fixed process funding bug.

---
+++ pkg/config/configuration.go
@@ -0,0 +1,241 @@
+package config
+
+import (
+	"fmt"
+	"github.com/plaid/plaid-go/plaid"
+	"github.com/spf13/viper"
+)
+
+const EnvironmentPrefix = "MONETR"
+
+type PlaidEnvironment string
+
+const (
+	PlaidSandbox     PlaidEnvironment = "sandbox"
+	PlaidDevelopment PlaidEnvironment = "development"
+	PlaidProduction  PlaidEnvironment = "production"
+)
+
+type SendGridTemplate string
+
+const (
+	VerifyEmailAddressTemplate SendGridTemplate = "verifyEmailTemplate"
+	ForgotPasswordTemplate     SendGridTemplate = "forgotPasswordTemplate"
+)
+
+type Configuration struct {
+	Name          string
+	Environment   string
+	UIDomainName  string
+	APIDomainName string
+	AllowSignUp   bool
+	Beta          Beta
+	CORS          CORS
+	JWT           JWT
+	Logging       Logging
+	Plaid         Plaid
+	PostgreSQL    PostgreSQL
+	ReCAPTCHA     ReCAPTCHA
+	Redis         Redis
+	SMTP          SMTPClient
+	SendGrid      SendGrid
+	Sentry        Sentry
+	Stripe        Stripe
+}
+
+type Beta struct {
+	EnableBetaCodes bool
+}
+
+type JWT struct {
+	LoginJwtSecret        string
+	RegistrationJwtSecret string
+}
+
+type PostgreSQL struct {
+	Address  string
+	Port     int
+	Username string
+	Password string
+	Database string
+}
+
+type SMTPClient struct {
+	Enabled  bool
+	Identity string
+	Username string
+	Password string
+	Host     string
+	Port     int
+
+	VerifyEmails bool
+}
+
+type SendGrid struct {
+	Enabled   bool
+	APIKey    string
+	Templates map[SendGridTemplate]string
+}
+
+type ReCAPTCHA struct {
+	Enabled    bool
+	PublicKey  string
+	PrivateKey string
+	Version    int // Currently only version 2 is supported by the UI.
+
+	VerifyLogin    bool
+	VerifyRegister bool
+}
+
+type Plaid struct {
+	ClientID     string
+	ClientSecret string
+	Environment  plaid.Environment
+	// This does not seem to be a scope within the documentation. Per the
+	// documentation "balance is not a valid product" and is enabled
+	// automatically. It is not clear if that includes this beta feature though.
+	EnableBalanceTransfers bool
+
+	// EnableReturningUserExperience changes the required data for sign up. If
+	// this is enabled then the user must provide their full legal name as well
+	// as their phone number.
+	// If enabled; email address and phone number verification is REQUIRED.
+	EnableReturningUserExperience bool
+
+	// EnableBirthdatePrompt will allow users to provide their birthday during
+	// sign up or afterwards in their user settings. This is used by plaid for
+	// future products. At the time of writing this it does not do anything.
+	EnableBirthdatePrompt bool
+
+	WebhooksEnabled bool
+	WebhooksDomain  string
+}
+
+type CORS struct {
+	AllowedOrigins []string
+	Debug          bool
+}
+
+// Redis defines the config used to connect to a redis for our worker pool. If these are left blank or default then we
+// will instead use a mock redis pool that is internal only. This is fine for single instance deployments, but anytime
+// more than one instance of the API is running a redis instance will be required.
+type Redis struct {
+	Enabled   bool
+	Address   string
+	Port      int
+	Namespace string
+}
+
+type Logging struct {
+	Level string
+}
+
+type Sentry struct {
+	Enabled         bool
+	DSN             string
+	SampleRate      float64
+	TraceSampleRate float64
+}
+
+type Stripe struct {
+	Enabled         bool
+	APIKey          string
+	PublicKey       string
+	WebhooksEnabled bool
+	WebhooksDomain  string
+	WebhookSecret   string
+	Prices          []string
+	Plans           []Plan
+	BillingEnabled  bool
+}
+
+func LoadConfiguration(configFilePath *string) Configuration {
+	v := viper.GetViper()
+
+	v.SetEnvPrefix(EnvironmentPrefix)
+	v.AutomaticEnv()
+
+	setupDefaults(v)
+	setupEnv(v)
+
+	if configFilePath != nil {
+		viper.SetConfigName(*configFilePath)
+	} else {
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath("/etc/monetr/")
+		viper.AddConfigPath(".")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("failed to read in config from file: %+v\n", err)
+	}
+
+	var config Configuration
+	if err := viper.Unmarshal(&config); err != nil {
+		panic(err)
+	}
+
+	return config
+}
+
+func setupDefaults(v *viper.Viper) {
+	v.SetDefault("Name", "monetr")
+	v.SetDefault("Environment", "development")
+	v.SetDefault("UIDomainName", "localhost:3000")
+	v.SetDefault("APIDomainName", "localhost:4000")
+	v.SetDefault("AllowSignUp", true)
+	v.SetDefault("PostgreSQL.Port", 5432)
+	v.SetDefault("PostgreSQL.Address", "localhost")
+	v.SetDefault("PostgreSQL.Username", "postgres")
+	v.SetDefault("PostgreSQL.Database", "postgres")
+	v.SetDefault("SMTP.Enabled", false)
+	v.SetDefault("ReCAPTCHA.Enabled", false)
+	v.SetDefault("Logging.Level", "info")
+}
+
+func setupEnv(v *viper.Viper) {
+	v.BindEnv("Name", "MONETR_NAME")
+	v.BindEnv("Environment", "MONETR_ENVIRONMENT")
+	v.BindEnv("UIDomainName", "MONETR_UI_DOMAIN_NAME")
+	v.BindEnv("APIDomainName", "MONETR_API_DOMAIN_NAME")
+	v.BindEnv("AllowSignUp", "MONETR_ALLOW_SIGN_UP")
+	v.BindEnv("EnableWebhooks", "MONETR_ENABLE_WEBHOOKS")
+	v.BindEnv("Beta.EnableBetaCodes", "MONETR_ENABLE_BETA_CODES")
+	v.BindEnv("Cors.AllowedOrigins", "MONETR_CORS_ALLOWED_ORIGINS")
+	v.BindEnv("Cors.Debug", "MONETR_CORS_DEBUG")
+	v.BindEnv("JWT.LoginJwtSecret", "MONETR_JWT_LOGIN_SECRET")
+	v.BindEnv("JWT.RegistrationJwtSecret", "MONETR_JWT_REGISTRATION_SECRET")
+	v.BindEnv("Logging.Level", "MONETR_LOG_LEVEL")
+	v.BindEnv("Plaid.ClientID", "MONETR_PLAID_CLIENT_ID")
+	v.BindEnv("Plaid.ClientSecret", "MONETR_PLAID_CLIENT_SECRET")
+	v.BindEnv("Plaid.Environment", "MONETR_PLAID_ENVIRONMENT")
+	v.BindEnv("Plaid.EnableBirthdatePrompt", "MONETR_PLAID_BIRTHDATE_PROMPT")
+	v.BindEnv("Plaid.EnableReturningUserExperience", "MONETR_PLAID_RETURNING_EXPERIENCE")
+	v.BindEnv("Plaid.WebhooksEnabled", "MONETR_PLAID_WEBHOOKS_ENABLED")
+	v.BindEnv("Plaid.WebhooksDomain", "MONETR_PLAID_WEBHOOKS_DOMAIN")
+	v.BindEnv("PostgreSQL.Address", "MONETR_PG_ADDRESS")
+	v.BindEnv("PostgreSQL.Port", "MONETR_PG_PORT")
+	v.BindEnv("PostgreSQL.Username", "MONETR_PG_USERNAME")
+	v.BindEnv("PostgreSQL.Password", "MONETR_PG_PASSWORD")
+	v.BindEnv("PostgreSQL.Database", "MONETR_PG_DATABASE")
+	v.BindEnv("ReCAPTCHA.Enabled", "MONETR_CAPTCHA_ENABLED")
+	v.BindEnv("ReCAPTCHA.PublicKey", "MONETR_CAPTCHA_PUBLIC_KEY")
+	v.BindEnv("ReCAPTCHA.PrivateKey", "MONETR_CAPTCHA_PRIVATE_KEY")
+	v.BindEnv("ReCAPTCHA.VerifyLogin", "MONETR_CAPTCHA_VERIFY_LOGIN")
+	v.BindEnv("ReCAPTCHA.VerifyRegister", "MONETR_CAPTCHA_VERIFY_REGISTER")
+	v.BindEnv("Redis.Enabled", "MONETR_REDIS_ENABLED")
+	v.BindEnv("Redis.Address", "MONETR_REDIS_ADDRESS")
+	v.BindEnv("Redis.Port", "MONETR_REDIS_PORT")
+	v.BindEnv("Redis.Namespace", "MONETR_REDIS_NAMESPACE")
+	v.BindEnv("Sentry.Enabled", "MONETR_SENTRY_ENABLED")
+	v.BindEnv("Sentry.DSN", "MONETR_SENTRY_DSN")
+	v.BindEnv("Sentry.SampleRate", "MONETR_SENTRY_SAMPLE_RATE")
+	v.BindEnv("Sentry.TraceSampleRate", "MONETR_SENTRY_TRACE_SAMPLE_RATE")
+	v.BindEnv("Stripe.Enabled", "MONETR_STRIPE_ENABLED")
+	v.BindEnv("Stripe.APIKey", "MONETR_STRIPE_API_KEY")
+	v.BindEnv("Stripe.PublicKey", "MONETR_STRIPE_PUBLIC_KEY")
+	v.BindEnv("Stripe.WebhooksEnabled", "MONETR_STRIPE_WEBHOOKS_ENABLED")
+	v.BindEnv("Stripe.WebhooksDomain", "MONETR_STRIPE_WEBHOOKS_DOMAIN")
+	v.BindEnv("Stripe.WebhookSecret", "MONETR_STRIPE_WEBHOOK_SECRET")
+}
---
+++ pkg/repository/repository.go
@@ -0,0 +1,143 @@
+package repository
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/monetrapp/rest-api/pkg/models"
+	"github.com/pkg/errors"
+)
+
+type Repository interface {
+	AccountId() uint64
+	UserId() uint64
+
+	AddExpenseToTransaction(ctx context.Context, transaction *models.Transaction, spending *models.Spending) error
+	CreateBankAccounts(bankAccounts ...models.BankAccount) error
+	CreateFundingSchedule(fundingSchedule *models.FundingSchedule) error
+	CreateLink(link *models.Link) error
+	CreatePlaidLink(link *models.PlaidLink) error
+	CreateSpending(expense *models.Spending) error
+	CreateTransaction(bankAccountId uint64, transaction *models.Transaction) error
+	DeleteSpending(ctx context.Context, bankAccountId, spendingId uint64) error
+	DeleteTransaction(ctx context.Context, bankAccountId, transactionId uint64) error
+	GetAccount() (*models.Account, error)
+	GetBalances(ctx context.Context, bankAccountId uint64) (*Balances, error)
+	GetBankAccount(bankAccountId uint64) (*models.BankAccount, error)
+	GetBankAccounts() ([]models.BankAccount, error)
+	GetBankAccountsByLinkId(linkId uint64) ([]models.BankAccount, error)
+	GetFundingSchedule(bankAccountId, fundingScheduleId uint64) (*models.FundingSchedule, error)
+	GetFundingSchedules(bankAccountId uint64) ([]models.FundingSchedule, error)
+	GetFundingStats(ctx context.Context, bankAccountId uint64) (*FundingStats, error)
+	GetIsSetup() (bool, error)
+	GetJob(jobId string) (models.Job, error)
+	GetLink(ctx context.Context, linkId uint64) (*models.Link, error)
+	GetLinkIsManual(linkId uint64) (bool, error)
+	GetLinkIsManualByBankAccountId(bankAccountId uint64) (bool, error)
+	GetLinks() ([]models.Link, error)
+	GetMe() (*models.User, error)
+	GetPendingTransactionsForBankAccount(bankAccountId uint64) ([]models.Transaction, error)
+	GetSpending(ctx context.Context, bankAccountId uint64) ([]models.Spending, error)
+	GetSpendingByFundingSchedule(bankAccountId, fundingScheduleId uint64) ([]models.Spending, error)
+	GetSpendingById(bankAccountId, expenseId uint64) (*models.Spending, error)
+	GetTransaction(bankAccountId, transactionId uint64) (*models.Transaction, error)
+	GetTransactions(bankAccountId uint64, limit, offset int) ([]models.Transaction, error)
+	GetTransactionsByPlaidId(linkId uint64, plaidTransactionIds []string) (map[string]models.Transaction, error)
+	GetTransactionsByPlaidTransactionId(ctx context.Context, linkId uint64, plaidTransactionIds []string) ([]models.Transaction, error)
+	InsertTransactions(ctx context.Context, transactions []models.Transaction) error
+	ProcessTransactionSpentFrom(ctx context.Context, bankAccountId uint64, input, existing *models.Transaction) (updatedExpenses []models.Spending, _ error)
+	UpdateBankAccounts(accounts []models.BankAccount) error
+	UpdateExpenses(bankAccountId uint64, updates []models.Spending) error
+	UpdateLink(link *models.Link) error
+	UpdateNextFundingScheduleDate(fundingScheduleId uint64, nextOccurrence time.Time) error
+	UpdateTransaction(bankAccountId uint64, transaction *models.Transaction) error
+
+	// UpdateTransactions is unique in that it REQUIRES that all data on each transaction object be populated. It is
+	// doing a bulk update, so if data is missing it has the potential to overwrite a transaction incorrectly.
+	UpdateTransactions(ctx context.Context, transactions []*models.Transaction) error
+
+	/*
+		Billing methods
+	*/
+
+	GetActiveSubscription(ctx context.Context) (*models.Subscription, error)
+}
+
+type UnauthenticatedRepository interface {
+	CreateLogin(email, hashedPassword string, firstName, lastName string, isEnabled bool) (*models.Login, error)
+	CreateAccount(timezone *time.Location) (*models.Account, error)
+	CreateUser(loginId, accountId uint64, user *models.User) error
+
+	// VerifyRegistration takes a registrationId and will finalize the registration record. If the registration has
+	// already been completed an error is returned.
+	VerifyRegistration(registrationId string) (*models.User, error)
+	GetLinksForItem(itemId string) (*models.Link, error)
+	ValidateBetaCode(ctx context.Context, betaCode string) (*models.Beta, error)
+	UseBetaCode(ctx context.Context, betaId, usedBy uint64) error
+}
+
+func NewRepositoryFromSession(userId, accountId uint64, database pg.DBI) Repository {
+	return &repositoryBase{
+		userId:    userId,
+		accountId: accountId,
+		txn:       database,
+	}
+}
+
+func NewUnauthenticatedRepository(txn *pg.Tx) UnauthenticatedRepository {
+	return &unauthenticatedRepo{
+		txn: txn,
+	}
+}
+
+var (
+	_ Repository = &repositoryBase{}
+)
+
+func (r *repositoryBase) UserId() uint64 {
+	return r.userId
+}
+
+func (r *repositoryBase) AccountId() uint64 {
+	return r.accountId
+}
+
+func (r *repositoryBase) AccountIdStr() string {
+	return strconv.FormatUint(r.AccountId(), 10)
+}
+
+func (r *repositoryBase) GetMe() (*models.User, error) {
+	var user models.User
+	err := r.txn.Model(&user).
+		Relation("Login").
+		Relation("Account").
+		Where(`"user"."user_id" = ? AND "user"."account_id" = ?`, r.userId, r.accountId).
+		Limit(1).
+		Select(&user)
+	switch err {
+	case pg.ErrNoRows:
+		return nil, errors.Errorf("user does not exist")
+	case nil:
+		break
+	default:
+		return nil, errors.Wrapf(err, "failed to retrieve user")
+	}
+
+	return &user, nil
+}
+
+func (r *repositoryBase) GetIsSetup() (bool, error) {
+	return r.txn.Model(&models.Link{}).
+		Where(`"link"."account_id" = ?`, r.accountId).
+		Exists()
+}
+
+func (r *repositoryBase) GetBankAccounts() ([]models.BankAccount, error) {
+	var result []models.BankAccount
+	err := r.txn.Model(&result).
+		Where(`"bank_account"."account_id" = ?`, r.AccountId()).
+		Select(&result)
+	return result, errors.Wrap(err, "failed to retrieve bank accounts")
+}
