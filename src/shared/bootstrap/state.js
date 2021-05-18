4d7cc8c811d836e19da8a2d12312b05e763ef971
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-05-18 18:17:38 +0000

    Update dependency @types/react to v17.0.6

---
+++ src/shared/bootstrap/state.js
@@ -0,0 +1,18 @@
+import { Record } from 'immutable';
+
+export default class BootstrapState extends Record({
+  apiUrl: '',
+  isReady: false,
+  isBootstrapping: true,
+  verifyLogin: false,
+  verifyRegister: false,
+  requireLegalName: false,
+  requirePhoneNumber: false,
+  ReCAPTCHAKey: null,
+  allowSignUp: false,
+  allowForgotPassword: false,
+  requireBetaCode: false,
+  stripePublicKey: '',
+}) {
+
+}
