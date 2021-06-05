0ad9ad0518551e23c0bec442c495eea529d799d4
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-06-05 14:51:41 -0500

    Messing around with SMTP emails.

---
+++ pkg/internal/fixtures/bank_account.go
@@ -0,0 +1,21 @@
+package fixtures
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/monetrapp/rest-api/pkg/models"
+	"testing"
+)
+
+func BankAccountFixture(t *testing.T) *models.BankAccount {
+	return &models.BankAccount{
+		PlaidAccountId:    gofakeit.UUID(),
+		AvailableBalance:  100000,
+		CurrentBalance:    98500,
+		Mask:              "0123",
+		Name:              "Personal Checking",
+		PlaidName:         "Checking",
+		PlaidOfficialName: "Checking",
+		Type:              "depository",
+		SubType:           "checking",
+	}
+}
