e9954ceb780bc533ea380e49fa8957d285210f5b
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-06-09 13:40:09 +0000

    Update dependency redoc to v2.0.0-rc.54

--- src/shared/balances/selectors/getBalances.ts
+++
@@ -1,2 +0,0 @@
-
-export const getBalances = state => state.balances.items;
--- src/testutils/store.ts
+++
@@ -1,6 +0,0 @@
-import { Store } from "redux";
-import configureStore from "store";
-
-export function createTestStore(initialState?: any): Store<any, any> {
-  return configureStore(initialState)
-}
