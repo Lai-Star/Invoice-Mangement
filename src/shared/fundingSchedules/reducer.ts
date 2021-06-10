0aa509d4fc592e606c01086e43893312dcfe1026
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-06-10 14:15:13 +0000

    Update babel monorepo to v7.14.5

---
+++ src/shared/fundingSchedules/reducer.ts
@@ -0,0 +1,41 @@
+import { LOGOUT } from "shared/authentication/actions";
+import { CreateFundingSchedule, FetchFundingSchedules, FundingScheduleActions } from "shared/fundingSchedules/actions";
+import FundingScheduleState from "shared/fundingSchedules/state";
+
+export default function reducer(state: FundingScheduleState = new FundingScheduleState(), action: FundingScheduleActions): FundingScheduleState {
+  switch (action.type) {
+    case CreateFundingSchedule.Request:
+    case FetchFundingSchedules.Request:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FetchFundingSchedules.Failure:
+    case CreateFundingSchedule.Failure:
+      return {
+        ...state,
+        loading: false,
+      };
+    case FetchFundingSchedules.Success:
+      return {
+        ...state,
+        loading: false,
+        items: state.items.mergeDeep(action.payload),
+      };
+    case CreateFundingSchedule.Success:
+      // With create funding schedule the payload will be a single funding schedule that was just created. So we can
+      // just do a set in for the current items to add the new schedule.
+      return {
+        ...state,
+        loading: false,
+        items: state.items.setIn([
+          action.payload.bankAccountId,
+          action.payload.fundingScheduleId,
+        ], action.payload),
+      };
+    case LOGOUT:
+      return new FundingScheduleState();
+    default:
+      return state;
+  }
+}
