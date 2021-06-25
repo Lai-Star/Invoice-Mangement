91a276e0c93b57cbc226052fb0296b649e96fe4f
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-06-25 12:21:59 +0000

    Update module github.com/spf13/viper to v1.8.1

---
+++ pkg/cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/monetrapp/rest-api/pkg/build"
+	"github.com/monetrapp/rest-api/pkg/internal/cmd"
+)
+
+var (
+	buildRevision = ""
+	buildtime     = ""
+	release       = ""
+)
+
+func main() {
+	build.Revision = buildRevision
+	build.BuildTime = buildtime
+	build.Release = release
+	// This is going to be the final actual program that is distributed.
+	if err := cmd.RootCommand.Execute(); err != nil {
+		log.Fatalf("failed: %+v", err)
+	}
+}
