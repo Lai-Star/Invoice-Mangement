5c5c84a823eaa14d61b46a2a191a5814b497d8d1
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-06-30 04:24:37 +0000

    Update dependency terser-webpack-plugin to v5.1.4

---
+++ src/shared/util/reducer.js
@@ -0,0 +1,12 @@
+export default function reducer(state = {}, action) {
+  switch (action.type) {
+    case 'API_CLIENT_SETUP':
+      return {
+        ...state,
+        setup: true,
+      };
+
+
+  }
+  return state;
+}
