2d93b571656a2617f88f552603f69972ea37c538
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-06-30 12:05:15 +0000

    Update module github.com/go-pg/pg/v10 to v10.10.1

---
+++ pkg/internal/fixtures/link.go
@@ -0,0 +1,32 @@
+package fixtures
+
+import (
+	"fmt"
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/monetrapp/rest-api/pkg/models"
+	"testing"
+)
+
+func ManualLink(t *testing.T) *models.Link {
+	return &models.Link{
+		LinkType:              models.ManualLinkType,
+		InstitutionName:       fmt.Sprintf("%s Bank", gofakeit.Company()),
+		CustomInstitutionName: "Personal Bank",
+	}
+}
+
+func PlaidLink(t *testing.T) (*models.Link, *models.PlaidLink) {
+	return &models.Link{
+			LinkType:              models.ManualLinkType,
+			InstitutionName:       fmt.Sprintf("%s Bank", gofakeit.Company()),
+			CustomInstitutionName: "Personal Bank",
+		}, &models.PlaidLink{
+			ItemId:      gofakeit.UUID(),
+			Products: []string{
+				"transactions",
+			},
+			WebhookUrl:      "",
+			InstitutionId:   "1234",
+			InstitutionName: gofakeit.Company(),
+		}
+}
