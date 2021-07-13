8878163f481c84a2b0457675c4623d533c28a8d0
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-07-13 23:18:46 +0000

    Update module github.com/hashicorp/vault/api to v1.1.1

---
+++ pkg/repository/funding_schedules.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"github.com/monetr/rest-api/pkg/models"
+	"github.com/pkg/errors"
+	"time"
+)
+
+func (r *repositoryBase) GetFundingSchedules(bankAccountId uint64) ([]models.FundingSchedule, error) {
+	result := make([]models.FundingSchedule, 0)
+	err := r.txn.Model(&result).
+		Where(`"funding_schedule"."account_id" = ?`, r.AccountId()).
+		Where(`"funding_schedule"."bank_account_id" = ?`, bankAccountId).
+		Select(&result)
+	return result, errors.Wrap(err, "failed to retrieve funding schedules")
+}
+
+func (r *repositoryBase) GetFundingSchedule(bankAccountId, fundingScheduleId uint64) (*models.FundingSchedule, error) {
+	var result models.FundingSchedule
+	err := r.txn.Model(&result).
+		Where(`"funding_schedule"."account_id" = ?`, r.AccountId()).
+		Where(`"funding_schedule"."bank_account_id" = ?`, bankAccountId).
+		Where(`"funding_schedule"."funding_schedule_id" = ?`, fundingScheduleId).
+		Limit(1).
+		Select(&result)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not retrieve funding schedule")
+	}
+
+	return &result, nil
+}
+
+func (r *repositoryBase) CreateFundingSchedule(fundingSchedule *models.FundingSchedule) error {
+	fundingSchedule.AccountId = r.AccountId()
+	_, err := r.txn.Model(fundingSchedule).
+		Insert(fundingSchedule)
+
+	return errors.Wrap(err, "failed to create funding schedule")
+}
+
+func (r *repositoryBase) UpdateNextFundingScheduleDate(fundingScheduleId uint64, nextOccurrence time.Time) error {
+	_, err := r.txn.Model(&models.FundingSchedule{}).
+		Set(`"next_occurrence" = ?`, nextOccurrence).
+		Where(`"funding_schedule"."account_id" = ?`, r.AccountId()).
+		Where(`"funding_schedule"."funding_schedule_id" = ?`, fundingScheduleId).
+		Update()
+	return errors.Wrap(err, "failed to set next occurrence")
+}
