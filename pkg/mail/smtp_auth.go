5e6a5e7078820dcddb1c857297f37a2871025103
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-07-31 16:41:13 -0500

    Tweaking makefile.

---
+++ pkg/mail/smtp_auth.go
@@ -0,0 +1,11 @@
+// +build !mini
+
+package mail
+
+import (
+	"net/smtp"
+)
+
+func PlainAuth(identity, username, password, host string) smtp.Auth {
+	return smtp.PlainAuth(identity, username, password, host)
+}
