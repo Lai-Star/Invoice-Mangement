92a5be4d4891164a4092a62aa3e5b0be0b2f50b5
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-08-27 18:24:04 +0000

    Update module github.com/stripe/stripe-go/v72 to v72.62.0

---
+++ pkg/internal/testutils/plaid.go
@@ -0,0 +1,12 @@
+package testutils
+
+import (
+	"github.com/monetr/rest-api/pkg/models"
+	"github.com/plaid/plaid-go/plaid"
+)
+
+type MockPlaidData struct {
+	PlaidTokens  map[string]models.PlaidToken
+	PlaidLinks   map[string]models.PlaidLink
+	BankAccounts map[string]map[string]plaid.Account
+}
