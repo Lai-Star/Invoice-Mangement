33463bf0c5ee588667cdd55b42ac7ac754be310a
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2021-10-03 14:22:56 -0500

    Fixing deploy

---
+++ pkg/internal/mock_plaid/transactions_test.go
@@ -0,0 +1,38 @@
+package mock_plaid
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateTransactions(t *testing.T) {
+	t.Run("2 days", func(t *testing.T) {
+		numberOfTransactions := 600
+		bankAccounts := []string{
+			"1234",
+			"5678",
+		}
+		end := time.Now()
+		start := time.Now().Add(-25 * time.Hour)
+		transactions := GenerateTransactions(t, start, end, numberOfTransactions, bankAccounts)
+		assert.Len(t, transactions, len(bankAccounts)*numberOfTransactions)
+		assert.Equal(t, end.Format("2006-01-02"), transactions[0].GetDate(), "date of first transaction should be end")
+		assert.Equal(t, start.Format("2006-01-02"), transactions[len(transactions)-1].GetDate(), "date of last transaction should be start")
+	})
+
+	t.Run("30 days", func(t *testing.T) {
+		numberOfTransactions := 3000
+		bankAccounts := []string{
+			"1234",
+			"5678",
+		}
+		end := time.Now().UTC()
+		start := time.Now().UTC().Add(-30 * 24 * time.Hour)
+		transactions := GenerateTransactions(t, start, end, numberOfTransactions, bankAccounts)
+		assert.Len(t, transactions, len(bankAccounts)*numberOfTransactions)
+		assert.Equal(t, end.Format("2006-01-02"), transactions[0].GetDate(), "date of first transaction should be end")
+		assert.Equal(t, start.Format("2006-01-02"), transactions[len(transactions)-1].GetDate(), "date of last transaction should be start")
+	})
+}
---
+++ pkg/internal/myownsanity/strings_test.go
@@ -0,0 +1,64 @@
+package myownsanity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringP(t *testing.T) {
+	var input string = "12345"
+	result := StringP(input)
+	assert.NotNil(t, result, "resulting pointer should never be nil")
+	assert.Equal(t, input, *result, "and the underlying value should match the input")
+}
+
+func TestStringDefault(t *testing.T) {
+	t.Run("value", func(t *testing.T) {
+		inputString := "I am a string"
+		defaultString := "I am the default string"
+		result := StringDefault(&inputString, defaultString)
+		assert.Equal(t, inputString, result, "result should match the input string")
+		assert.NotEqual(t, defaultString, result, "and should definitely not match the default string")
+	})
+
+	t.Run("nil", func(t *testing.T) {
+		defaultString := "I am stil the default string"
+		result := StringDefault(nil, defaultString)
+		assert.Equal(t, defaultString, result, "when the input is nil, the default should be returned")
+	})
+}
+
+func TestSliceContains(t *testing.T) {
+	data := []string{
+		"Item #1",
+		"Item #2",
+	}
+
+	assert.True(t, SliceContains(data, "Item #1"), "should contain item #1")
+	assert.False(t, SliceContains(data, "Item #3"), "should contain item #3")
+}
+
+func TestStringPEqual(t *testing.T) {
+	{
+		var a, b string = "a", "b"
+		assert.False(t, StringPEqual(&a, &b), "should not be equal")
+	}
+
+	{
+		var a, b string = "a", "a"
+		assert.True(t, StringPEqual(&a, &b), "should be equal")
+	}
+
+	{
+		a := "a"
+		assert.False(t, StringPEqual(&a, nil), "should not be equal")
+	}
+
+	{
+		b := "b"
+		assert.False(t, StringPEqual(nil, &b), "should not be equal")
+	}
+
+	assert.True(t, StringPEqual(nil, nil), "should be equal")
+}
